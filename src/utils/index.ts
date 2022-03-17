3640778ddbcfd910baedff6513dad94ce6e7590e
d_ttang <d_ttang@163.com>
2022-03-17 20:24:18 +0800

    feat: collections

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,6 +7,13 @@ export const isProd = false // process.env.NODE_ENV === 'production'
 export const logIsProd = process.env.NODE_ENV === 'production'
 
 // explore 数据源
-export const collections = isProd ? [] : ['AhY7v1fSv4c7P9ouhkfWHDzv7okRNo2TUfwDxwxHZXfb']
+export const collections = isProd
+  ? []
+  : [
+      'AhY7v1fSv4c7P9ouhkfWHDzv7okRNo2TUfwDxwxHZXfb',
+      'DaFBQUghKeSuuAjgZWLkZfHzVs5K8ELf1oGXKkkupwDX',
+      '4j4SqfJv4Qf4yJbLxQxLKvT2TV4eqMo5A5AuNxqr1AuL',
+      'U7KxNoZ3rNbtG5DgRYQQsjs2dWoSSLLRLjit6VXRyzR',
+    ]
 
 export default {}
