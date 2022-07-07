ebe2f2e3db352c784ea1a9e7b739680118299211
Hulk <33158585+byteHulk@users.noreply.github.com>
2022-07-07 10:36:46 +0800

    [fix] fix deploy bug

--- apps/mini-site/baatarverse/next.config.js
+++ apps/mini-site/baatarverse/next.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withNx = require('@nrwl/next/plugins/with-nx');
+// const withNx = require('@nrwl/next/plugins/with-nx');
 const withVideos = require('next-videos')
  
 /**
@@ -11,6 +11,11 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 };
 
-module.exports = withNx(withVideos(nextConfig));
+module.exports = withVideos(nextConfig);
+// module.exports = withNx(withVideos(nextConfig));
