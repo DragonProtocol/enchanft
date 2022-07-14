1800e266982557acb6cac8e4b06b371aebf58c84
Hulk <33158585+byteHulk@users.noreply.github.com>
2022-07-14 19:20:50 +0800

    [feat] update hash params fun

--- apps/collector-page/src/container/CallBack.tsx
+++ apps/collector-page/src/container/CallBack.tsx
@@ -21,8 +21,7 @@ const CallBack: React.FC = (props) => {
   const { twitter, errorMsg } = useAppSelector(selectAccount)
 
   useEffect(() => {
-    // const urlParams = new URLSearchParams(location.search)
-    const code = location.hash.match(/code=([^&]*)/)?.[1]
+    [feat] update hash params fun    const code = location.hash.match(/code=([^&]*)/)?.[1]
     // const urlParams = new URLSearchParams(location.search)
     // const code = urlParams.get('code')
     if (code) {
