d8af9d8d97faeb4e67bdaaa91a854b64286eaca8
bufan <bufan@hotmail.com>
2022-07-29 15:41:31 +0800

    add ref link function

--- apps/collector-page/src/container/Ref.tsx
+++ apps/collector-page/src/container/Ref.tsx
@@ -1,7 +1,14 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
+export const getTakeTaskRefLink = (referrerId:number,type:RefType,taskID:number):string =>{
+  return generateRefLink(referrerId,type,{taskID:taskID})
+}
 
+const generateRefLink = (referrerId:number,type:RefType,data:RefData):string =>{
+  const refCode = enRefCode({referrerId:referrerId, type:type, data:data})
+  return REF_LINK_PREFIX+refCode
+}
 
 export default function Ref() {
   const { refCode } = useParams()
@@ -9,12 +16,12 @@ export default function Ref() {
   useEffect(() => {
     if (!refCode) return
     // console.log('ref code: ', refCode, window.atob(refCode))
-    const refInfo: RefInfo = JSON.parse(window.atob(refCode))
+    const refInfo: RefInfo = deRefCode(refCode)
     console.log('ref info: ', refInfo)
     saveRefInfo(refInfo)
     switch (refInfo.type) {
-      case REF_TYPE.TAKE_TASK:
-        navigate('/task/' + refInfo.data.taskID)
+      case RefType.TAKE_TASK:
+        navigate('/task/' + (refInfo.data as TakeTaskData).taskID)
         break;
     }
   }, [refCode])
@@ -24,20 +31,32 @@ export default function Ref() {
   )
 }
 
-export const saveRefInfo = (info: RefInfo) => {
+
+const enRefCode = (info: RefInfo) => {
+  return btoa(window.atob(JSON.stringify(info)))
+}
+
+const deRefCode = (refCode: string) => {
+  return JSON.parse(window.atob(refCode))
+}
+
+const saveRefInfo = (info: RefInfo) => {
   let infoKey: string = LOCAL_STORAGE_KEY_PREFIX + info.type
   switch (info.type) {
-    case REF_TYPE.TAKE_TASK:
-      infoKey = infoKey + ':' + info.data.taskID
+    case RefType.TAKE_TASK:
+      infoKey = infoKey + ':' + (info.data as TakeTaskData).taskID
+      break;
+    case RefType.TAKE_TASK:
+      infoKey = infoKey + ':' + (info.data as NotifacationRefData).communityID
       break;
   }
   localStorage.setItem(infoKey, JSON.stringify(info))
 }
 
-export const loadRefInfo = (type: REF_TYPE, filter: string): RefInfo | null => {
+const loadRefInfo = (type: RefType, filter: string): RefInfo | null => {
   let infoKey: string = LOCAL_STORAGE_KEY_PREFIX + type
   switch (type) {
-    case REF_TYPE.TAKE_TASK:
+    case RefType.TAKE_TASK:
       infoKey = infoKey + ':' + filter
       break;
   }
@@ -45,10 +64,10 @@ export const loadRefInfo = (type: REF_TYPE, filter: string): RefInfo | null => {
   return info ? JSON.parse(info) : null
 }
 
-export const removeRefInfo = (type: REF_TYPE, filter: string) => {
+const removeRefInfo = (type: RefType, filter: string) => {
   let infoKey: string = LOCAL_STORAGE_KEY_PREFIX + type
   switch (type) {
-    case REF_TYPE.TAKE_TASK:
+    case RefType.TAKE_TASK:
       infoKey = infoKey + ':' + filter
       break;
   }
@@ -57,23 +76,29 @@ export const removeRefInfo = (type: REF_TYPE, filter: string) => {
 
 export interface RefInfo {
   referrerId: number,
-  type: REF_TYPE,
-  data: TakeTaskData
+  type: RefType,
+  data: RefData
 }
 
-export enum REF_TYPE {
-  TAKE_TASK = 'takeTask',
-  TURN_ON_NOTIFICATION = 'turnOnNotification',
+export enum RefType {
+  TAKE_TASK = 'TAKE_TASK',
+  TURN_ON_NOTIFICATION = 'TURN_ON_NOTIFICATION', //NOT planed yet
 }
 
 const EXAMPLE_REF_INFO: RefInfo = {
   referrerId: 2,
-  type: REF_TYPE.TAKE_TASK,
+  type: RefType.TAKE_TASK,
   data: { taskID: 1 }
 }
+
+export type RefData = TakeTaskData | NotifacationRefData;
 
 export interface TakeTaskData {
   taskID: number
 }
+export interface NotifacationRefData {
+  communityID: number;
+}
 
 const LOCAL_STORAGE_KEY_PREFIX = 'ref:'
+const REF_LINK_PREFIX = '/ref/'
