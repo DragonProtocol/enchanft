48796b493353469921656befaf29079535e521c3
shixuewen <friendlysxw@163.com>
2022-08-08 18:33:40 +0800

    fix: add task image remote

--- apps/collector-page/src/components/business/task/TaskImageDefault.tsx
+++ apps/collector-page/src/components/business/task/TaskImageDefault.tsx
@@ -2,25 +2,17 @@
  * @Author: shixuewen friendlysxw@163.com
  * @Date: 2022-08-01 13:41:52
  * @LastEditors: shixuewen friendlysxw@163.com
- * @LastEditTime: 2022-08-08 17:00:19
+ * @LastEditTime: 2022-08-08 18:32:33
  * @Description: file description
  */
 import React, { HTMLAttributes } from 'react'
 import styled from 'styled-components'
-import default_image_1 from './imgs/default_image_1.png'
-import default_image_2 from './imgs/default_image_2.png'
-import default_image_3 from './imgs/default_image_3.png'
-import default_image_4 from './imgs/default_image_4.png'
+import { TASK_DEFAULT_IMAGE_URL } from '../../../constants'
 type TaskImageDefaultProps = HTMLAttributes<HTMLImageElement> & {
   src?: string
 }
 const TaskImageDefault: React.FC<TaskImageDefaultProps> = ({ src, ...otherProps }: TaskImageDefaultProps) => {
-  let imageSrc = src
-  if (!imageSrc) {
-    const images = [default_image_1, default_image_2, default_image_3, default_image_4]
-    imageSrc = images[Math.floor(Math.random() * images.length)]
-  }
-  return <TaskImageDefaultWrapper src={imageSrc} {...otherProps} />
+  return <TaskImageDefaultWrapper src={src || TASK_DEFAULT_IMAGE_URL} {...otherProps} />
 }
 export default TaskImageDefault
 const TaskImageDefaultWrapper = styled.img``
--- apps/collector-page/src/constants/index.ts
+++ apps/collector-page/src/constants/index.ts
@@ -2,7 +2,7 @@
  * @Author: shixuewen friendlysxw@163.com
  * @Date: 2022-07-01 15:09:50
  * @LastEditors: shixuewen friendlysxw@163.com
- * @LastEditTime: 2022-07-19 15:11:26
+ * @LastEditTime: 2022-08-08 18:30:28
  * @Description: 系统相关常量定义
  */
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
@@ -18,3 +18,5 @@ export const MEDIA_BREAK_POINTS = {
 export const MOBILE_BREAK_POINT = 768
 export const VIEW_LAMPORTS_DECIMAL = 3
 export const MAX_CHILDREN_PER_LEVEL = 3
+export const TASK_DEFAULT_IMAGE_URL =
+  'https://strapi-njmm.onrender.com/uploads/thumbnail_default_image_4_d5ad13d828.png'
