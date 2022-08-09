bfc7aec63b070c6174663d2241107aab4d3b33a4
liang <code@dswap.com>
2022-08-09 21:06:22 +0800

    fix var

--- apps/collector-page/src/utils/socialMedia.ts
+++ apps/collector-page/src/utils/socialMedia.ts
@@ -1,19 +1,23 @@
+const TWITTER_CLIENT_ID = process.env.TWITTER_CLIENT_ID；
+const TWITTER_CALLBACK_URL = process.env.TWITTER_CALLBACK_URL；
+const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+const DISCORD_CALLBACK_URL = process.env.DISCORD_CALLBACK_URL；
 export const connectionSocialMedia = (type: string) => {
   const socialMediaMap = {
     twitter: `https://twitter.com/i/oauth2/authorize?
 response_type=code&
-client_id=${process.env.TWITTER_CLIENT_ID}&
-redirect_uri=${process.env.TWITTER_CALLBACK_URL}&
+client_id=${TWITTER_CLIENT_ID}&
+redirect_uri=${TWITTER_CALLBACK_URL}&
 scope=bookmark.read+block.read+like.read+list.read+follows.read+space.read+mute.read+tweet.read+users.read+offline.access&
 state=3063390848298.8647&
 code_challenge=challenge&
 code_challenge_method=plain`,
     discord: `https://discord.com/oauth2/authorize?
 response_type=code&
-client_id=${process.env.DISCORD_CLIENT_ID}&
+client_id=${DISCORD_CLIENT_ID}&
 scope=identify%20guilds&
 state=15773059ghq9183habn&
-redirect_uri=${process.env.DISCORD_CALLBACK_URL}&
+redirect_uri=${DISCORD_CALLBACK_URL}&
 prompt=consent`,
   }
 
