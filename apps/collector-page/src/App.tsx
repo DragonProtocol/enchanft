ac9b8160f2f740cc3417c6e848344d32427e321e
shixuewen <friendlysxw@163.com>
2022-08-23 19:40:18 +0800

    ci: prettier fix

--- apps/collector-page/src/App.tsx
+++ apps/collector-page/src/App.tsx
@@ -13,27 +13,21 @@ import Layout from './components/layout/Index'
 import { store } from './store/store'
 import GlobalStyle from './GlobalStyle'
 import { injectStore } from './request/axios'
-import Appsignal from "@appsignal/javascript";
-import { ErrorBoundary } from "@appsignal/react";
+import Appsignal from '@appsignal/javascript'
+import { ErrorBoundary } from '@appsignal/react'
 
 injectStore(store)
 import './mock/index'
 const appsignal = new Appsignal({
-  key: process.env.APPSIGNAL_FRONTEND_API_KEY
+  key: process.env.APPSIGNAL_FRONTEND_API_KEY,
 })
 
-const FallbackComponent = () => (
-  <div>Uh oh! There was an error :(</div>
-)
+const FallbackComponent = () => <div>Uh oh! There was an error :(</div>
 
 function App() {
   appsignal.demo()
   return (
-    <ErrorBoundary
-      instance={appsignal}
-      tags={{ tag: "value" }}
-      fallback={(error) => <FallbackComponent />}
-    >
+    <ErrorBoundary instance={appsignal} tags={{ tag: 'value' }} fallback={(error) => <FallbackComponent />}>
       <SolanaProvider>
         <SynftProvider>
           <ReduxProvider store={store}>
