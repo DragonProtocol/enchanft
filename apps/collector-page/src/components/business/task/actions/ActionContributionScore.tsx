a25a9a8c79343cb488e8c4f540fe77c4a122b03a
shixuewen <friendlysxw@163.com>
2022-09-01 18:06:53 +0800

    fix: fix ui

--- apps/collector-page/src/components/business/task/actions/ActionContributionScore.tsx
+++ apps/collector-page/src/components/business/task/actions/ActionContributionScore.tsx
@@ -2,7 +2,7 @@
  * @Author: shixuewen friendlysxw@163.com
  * @Date: 2022-07-25 15:33:48
  * @LastEditors: shixuewen friendlysxw@163.com
- * @LastEditTime: 2022-08-26 13:57:49
+ * @LastEditTime: 2022-09-01 18:04:53
  * @Description: file description
  */
 import React from 'react'
@@ -10,7 +10,7 @@ import styled, { css } from 'styled-components'
 import { UserActionStatus } from '../../../../types/api'
 import { TaskActionItemDataType } from '../TaskActionItem'
 import TooltipWrapper from '../../../common/tooltip/TooltipWrapper'
-import PngIconWL from '../../../common/icons/PngIconWL'
+import IconWL from '../../../common/icons/IconWL'
 import { useNavigate } from 'react-router-dom'
 import ActionNameSpan from './ActionNameSpan'
 import ActionIconBox from './ActionIconBox'
@@ -35,7 +35,7 @@ const ActionContributionScore: React.FC<ActionContributionScoreProps> = ({
       <ActionContributionScoreRow>
         <ActionIconBox allowHandle={allowHandle} isDone={isDone} onClick={handleAction}>
           <TooltipWrapper title={description}>
-            <PngIconWL style={{ opacity: isDone ? 0.5 : 1 }} />
+            <IconWL opacity={isDone ? 0.5 : 1} />
           </TooltipWrapper>
         </ActionIconBox>
         <ActionContentBox onClick={handleAction}>
