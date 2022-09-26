e2e0d9b900ed2b9dfd76ee5c893050e5a2b570b3
Hulk <33158585+byteHulk@users.noreply.github.com>
2022-09-26 10:59:50 +0800

    [fix] fix eslint

--- apps/mini-site/baatarverse/pages/_app.tsx
+++ apps/mini-site/baatarverse/pages/_app.tsx
@@ -4,20 +4,7 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 
-function useRem() {
-  const setRem = async () => {
-    await require('lib-flexible')
-  }
-  useEffect(() => {
-    setRem()
-    window.addEventListener('resize', setRem)
-  }, [])
-}
-
-
 function CustomApp({ Component, pageProps }: AppProps) {
-  // useRem()
-
   return (
     <>
       <Head>
