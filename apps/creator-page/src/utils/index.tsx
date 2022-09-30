c71936513cd0c089027768f948c606caeed6ab58
ttang <ttang@ttang-MacBook-Pro-2021.local>
2022-09-30 13:26:46 +0800

    feat: fix float

--- apps/creator-page/src/utils/index.tsx
+++ apps/creator-page/src/utils/index.tsx
@@ -1,5 +1,5 @@
 export function numberInput(e: React.KeyboardEvent<HTMLInputElement>) {
-  if (e.charCode < 48 || e.charCode > 57) {
+  if (e.charCode !== 46 && (e.charCode < 48 || e.charCode > 57)) {
     e.preventDefault();
   }
 }
