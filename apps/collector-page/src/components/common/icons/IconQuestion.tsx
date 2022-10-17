396ba51d8257ef11824f6ecd011c4dbb7d78d3df
shixuewen <friendlysxw@163.com>
2022-10-17 16:16:47 +0800

    ci: prettier fix

--- apps/collector-page/src/components/common/icons/IconQuestion.tsx
+++ apps/collector-page/src/components/common/icons/IconQuestion.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { ReactComponent as CaretLeftIcon } from './svgs/question.svg';
+import React from 'react'
+import { ReactComponent as CaretLeftIcon } from './svgs/question.svg'
 interface Props {
-  size?: string;
-  opacity?: number;
+  size?: string
+  opacity?: number
 }
 const IconQuestion: React.FC<Props> = ({ size = '1.5rem', opacity = 1 }) => {
-  return <CaretLeftIcon width={size} height={size} opacity={opacity} />;
-};
-export default IconQuestion;
+  return <CaretLeftIcon width={size} height={size} opacity={opacity} />
+}
+export default IconQuestion
