672ea95391655fa2698947475f0ee5ed3482657a
shixuewen <friendlysxw@163.com>
2022-11-29 17:22:09 +0800

    fix: update baartarvse

--- apps/mini-site/baatarverse/.eslintrc.js
+++ apps/mini-site/baatarverse/.eslintrc.js
@@ -2,15 +2,15 @@
  * @Author: shixuewen friendlysxw@163.com
  * @Date: 2022-11-25 21:04:56
  * @LastEditors: shixuewen friendlysxw@163.com
- * @LastEditTime: 2022-11-29 16:51:53
+ * @LastEditTime: 2022-11-29 17:20:03
  * @Description: file description
  */
 module.exports = {
   root: true,
-  // extends: ['../../.eslintrc'],
+  extends: ['../../../.eslintrc'],
   // parserOptions: {
   //   tsconfigRootDir: __dirname,
   //   project: ['./tsconfig.json'],
   // },
-  ignorePatterns: ['*'],
+  ignorePatterns: ['.eslintrc.js'],
 };
