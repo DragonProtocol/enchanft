94f5ff646e5d01042ecfb678b4f5bf19e85d033f
shixuewen <friendlysxw@163.com>
2022-11-29 17:45:50 +0800

    fix: update tsconfig

--- apps/mini-site/baatarverse/.eslintrc.js
+++ apps/mini-site/baatarverse/.eslintrc.js
@@ -2,7 +2,7 @@
  * @Author: shixuewen friendlysxw@163.com
  * @Date: 2022-11-25 21:04:56
  * @LastEditors: shixuewen friendlysxw@163.com
- * @LastEditTime: 2022-11-29 17:20:03
+ * @LastEditTime: 2022-11-29 17:42:57
  * @Description: file description
  */
 module.exports = {
@@ -10,7 +10,7 @@ module.exports = {
   extends: ['../../../.eslintrc'],
   // parserOptions: {
   //   tsconfigRootDir: __dirname,
-  //   project: ['./tsconfig.json'],
+  //   project: ['./tsconfig.json', './tsconfig.spec.json'],
   // },
   ignorePatterns: ['.eslintrc.js'],
 };
