95f653583bf5db4994b70a297677b93a79299d6a
Hulk <33158585+byteHulk@users.noreply.github.com>
2022-12-09 09:15:50 +0800

    feat: add u3-chrome-extension pack

---
+++ apps/u3-chrome-extension/background.js
@@ -0,0 +1,25 @@
+chrome.action.onClicked.addListener(function () {
+  chrome.tabs.create({
+    url: chrome.runtime.getURL('index.html')
+  });
+});
+
+var HEADERS_TO_FILTER = ["Content-Security-Policy", "X-Frame-Options", "Frame-Options"];
+
+chrome.runtime.onInstalled.addListener(() => {
+  const RULE = {
+    id: 1,
+    condition: {
+      initiatorDomains: [chrome.runtime.id],
+      resourceTypes: ['main_frame', 'sub_frame'],
+    },
+    action: {
+      type: 'modifyHeaders',
+      responseHeaders: HEADERS_TO_FILTER.map(header => ({ header, operation: 'remove' }))
+    },
+  };
+  chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: [RULE.id],
+    addRules: [RULE],
+  });
+});
