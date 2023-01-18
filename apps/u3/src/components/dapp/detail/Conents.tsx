af8c0277a253a9c1cb358871b044861707d3b481
shixuewen <friendlysxw@163.com>
2023-01-18 18:38:03 +0800

    fix: update dapp detail contents

--- apps/u3/src/components/dapp/detail/Conents.tsx
+++ apps/u3/src/components/dapp/detail/Conents.tsx
@@ -2,7 +2,7 @@
  * @Author: shixuewen friendlysxw@163.com
  * @Date: 2023-01-18 17:12:51
  * @LastEditors: shixuewen friendlysxw@163.com
- * @LastEditTime: 2023-01-18 17:55:24
+ * @LastEditTime: 2023-01-18 18:37:10
  * @Description: file description
  */
 import styled, { StyledComponentPropsWithRef } from 'styled-components';
@@ -31,7 +31,8 @@ const ConentsWrapper = styled(Card)`
   min-height: 424px;
 `;
 const ConentsList = styled.div`
+  margin-top: 20px;
   display: grid;
   grid-gap: 20px;
-  grid-template-columns: repeat(4, minmax(200px, 1fr));
+  grid-template-columns: repeat(4, minmax(100px, 1fr));
 `;
