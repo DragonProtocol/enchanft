68c9bee35abf8d514a1a2dda3f6344d87d7cbfea
shixuewen <friendlysxw@163.com>
2023-01-29 18:39:31 +0800

    fix: dapp events contents order

--- apps/u3/src/container/Dapp.tsx
+++ apps/u3/src/container/Dapp.tsx
@@ -2,7 +2,7 @@
  * @Author: shixuewen friendlysxw@163.com
  * @Date: 2023-01-17 16:35:10
  * @LastEditors: shixuewen friendlysxw@163.com
- * @LastEditTime: 2023-01-29 18:10:38
+ * @LastEditTime: 2023-01-29 18:37:49
  * @Description: file description
  */
 import { useCallback, useEffect, useState } from 'react';
@@ -108,16 +108,16 @@ export default function Dapp() {
       <ContentLayout>
         <ContentLayoutLeft>
           <Screeshots />
-          {data?.events?.length && (
-            <Events
-              data={data.events}
-              onItemClick={(item) => navigate(`/events/${item.id}`)}
-            />
-          )}
           {data?.contents?.length && (
             <Conents
               data={data.contents}
               onItemClick={(item) => navigate(`/contents/${item.id}`)}
+            />
+          )}
+          {data?.events?.length && (
+            <Events
+              data={data.events}
+              onItemClick={(item) => navigate(`/events/${item.id}`)}
             />
           )}
 
